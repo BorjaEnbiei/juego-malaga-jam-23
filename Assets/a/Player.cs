6a29e77bcd67fd904cdfc7e6ede3949485cab7b6
jokaaaaaa <victorjc2002cadiz@gmail.com>
2023-02-04 00:11:08 +0100

    asse

---
+++ Assets/a/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private int vida;
+    [SerializeField] private int ataque;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/a/script_combate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_boton_combate : MonoBehaviour
+{
+    Player player;
+    // playerCard = GameObject.Find("playerCard");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = new Ge();
+        player.
+    }
+    void OnClick()
+    {
+        Debug.Log("Clcik");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
